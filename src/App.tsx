795dd7ef57bb57c9c97d340313b5c51be02a7bc3
majicmaj <majd.tarbin@outlook.com>
2025-03-19 08:32:06 -0400

    Implement POST request functionality and add mode toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,66 @@
 import { useState } from "react";
 import "./App.css";
+import { useMutation } from "@tanstack/react-query";
+import ModeToggle from "./components/system/modeToggle";
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+
+const usePostQuery = () => {
+  return useMutation({
+    mutationFn: async (query: string) => {
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: query }),
+      });
+      return response.json();
+    },
+  });
+};
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("");
+
+  const { mutate, data } = usePostQuery();
+  // mutate allows me to POST the query to the server
+  // data allows me to GET (read) the data returned in the response from the server
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    mutate(query); // POST request to the server
+  };
 
   return (
     <div className="grid place-items-center w-screen h-screen">
-      <div className="card">
-        <div className="card-body">
-          <h1 className="card-title">Vite + React</h1>
-          <div className="card">
-            <button
-              className="btn btn-primary"
-              onClick={() => setCount((count) => count + 1)}
-            >
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </div>
+      <ModeToggle />
+      <div>
+        <input
+          type="text"
+          placeholder="Type here"
+          className="p-2 border rounded px-4"
+          onChange={handleChange}
+          value={query}
+        />
+        <button
+          className="btn"
+          onClick={handleSubmit} // Call the handleSubmit function on button click
+        >
+          Submit
+        </button>
+      </div>
+      <div>
+        {data &&
+          data.map((item: { id: number; title: string }) => (
+            <div key={item.id} className="p-2 border rounded mt-2">
+              {item.title}
+            </div>
+          ))}
       </div>
     </div>
   );
